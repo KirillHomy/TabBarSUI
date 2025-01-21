2c8900a51b002057ffcc3571f6a5f6ed4716ab3b
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2025-01-21 20:48:15 +0200

    Initial Commit

---
+++ TabBarSUI/TabBarSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarSUIApp.swift
+//  TabBarSUI
+//
+//  Created by Kirill Khomicevich on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TabBarSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
